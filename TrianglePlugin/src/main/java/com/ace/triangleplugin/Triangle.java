5c4ad8ab8c5d11fcc815197acfd15d4e97739e86
Grigore Cosmin <50809946+CosminGRG@users.noreply.github.com>
2020-05-13 20:47:47 +0300

    Test

---
+++ TrianglePlugin/src/main/java/com/ace/triangleplugin/Triangle.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ace.triangleplugin;
+
+import com.ace.autoplugin.interfaces.*;
+import com.ace.menu.ConsoleController;
+import java.awt.Point;
+import java.util.Scanner;
+/**
+ *
+ * @author ykscr
+ */
+public class Triangle implements IShape{
+    public static String name = "Triangle";
+    
+    Point[] points = {
+        new Point(),
+        new Point(),
+        new Point()
+    };
+    
+    @Override
+    public void draw() {
+        for (Point point : points)
+        {
+            point.toString();
+        }
+    }
+    
+    @Override
+    public void read() {
+        @SuppressWarnings("resource")
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter 3 points:");
+        
+        for (int i = 0; i < points.length; i++)
+        {
+            System.out.println("Point: " + i);
+            System.out.println("X: ");
+            points[i].x = input.nextInt();
+            System.out.println("Y: ");
+            points[i].y = input.nextInt();
+        }
+
+        ConsoleController.Clear();
+        System.out.println("Shape added - " + name);
+        System.out.println("Triangle points coordinates: ");
+        for (Point point : points)
+        {
+            point.toString();
+        }
+        System.out.println("");
+    }
+    
+    @Override
+    public String getInfo() {
+        String info = name;
+        int i = 0;
+        for (Point point : points)
+        {
+            info += "- Point " + i + ": X =" + point.x + " Y = " + point.y;
+            i++;
+        }
+        return info;
+    }
+}
---
+++ TrianglePlugin/src/main/java/com/ace/triangleplugin/TrianglePlugin.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ace.triangleplugin;
+
+import com.ace.autoplugin.interfaces.IShape;
+import com.ace.autoplugin.interfaces.IPluginsAppPlugin;
+
+
+/**
+ *
+ * @author ykscr
+ */
+public class TrianglePlugin implements IPluginsAppPlugin{
+    @Override
+    public String getUniqueNameText() {
+        return "TrianglePlugin";
+    }
+
+    @Override
+    public String getDisplayText() {
+        return "Triangle";
+    }
+
+    @Override
+    public IShape getShape() {
+        return new Triangle();
+    }
+}
